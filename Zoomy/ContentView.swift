a78e84d99a9ddcf9aeed334df19a84e40e6fe1cb
Aneudys Amparo <aneudysamparo@gmail.com>
2024-02-14 15:10:25 -0400

    Initial Commit

---
+++ Zoomy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zoomy
+//
+//  Created by Aneudys Amparo on 14/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zoomy/ZoomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomyApp.swift
+//  Zoomy
+//
+//  Created by Aneudys Amparo on 14/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ZoomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
